4de1026f03f69e99469226bbc410e1705b025d4c
kunieda06 <n_kunieda@infoprax.co.jp>
2025-09-10 15:06:26 +0900

    課題修正_2

--- demo/src/main/resources/static/hantei.js
+++ demo/src/main/resources/static/hantei.js
@@ -1,11 +1,18 @@
 let rowCount = 0;
 
 const addBtn = document.getElementById("addbtn");
+const delBtn = document.getElementById("delbtn");
 const table = document.getElementById("tablePlan");
 const tableBody = document.getElementById("tableBody");
 
+function updateRowCountDisplay(){
+    const rowCountDisplay = document.getElementById("count");
+    rowCountDisplay.textContent = `追加 件数：${rowCount}件`;
+}
 
+updateRowCountDisplay();
 
+//追加ボタン
 addBtn.addEventListener("click",()=>{
     if (rowCount === 0) {
         table.style.display = "table";
@@ -25,17 +32,32 @@ addBtn.addEventListener("click",()=>{
     //判断内容
     const judgeCell = document.createElement("td");
     const judge = document.createElement("input");
-    input.type = "text";
-    input.placeholder = "予定の内容"
-    inputCell.appendChild(input);
-
-
-
-
+    judge.type = "text";
+    judge.readOnly = true;
+    judge.placeholder = "判断内容"
+    judgeCell.appendChild(judge);
 
     newRow.appendChild(checkCell);
     newRow.appendChild(inputCell);
+    newRow.appendChild(judgeCell);
 
     tableBody.appendChild(newRow);
   rowCount++;
+  updateRowCountDisplay();
+});
+
+//削除ボタン
+delBtn.addEventListener("click",()=>{
+    const rows = tableBody.querySelectorAll("tr");
+    rows.forEach(row => {
+        const checkbox = row.querySelector("input[type='checkbox']");
+        if(checkbox && checkbox.checked){
+            row.remove();
+            rowCount--;
+        }
+    });
+    if(rowCount===0){
+        table.style.display = "none";
+    }
+    updateRowCountDisplay();
 });
